9cf7d2addef55a87b8b6f1b2431af22ba0c28d82
lucieandres <lucie.andres@etu.unice.fr>
2021-11-25 10:54:56 +0100

    membre

---
+++ src/reseauSocial/core/Member.java
@@ -0,0 +1,26 @@
+package reseauSocial.core;
+
+public class Member implements MemberInterface{
+
+	String nom;
+	String localisation;
+	
+	@Override
+	public String getLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setLocation(String s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
