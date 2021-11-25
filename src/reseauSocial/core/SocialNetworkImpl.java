122b0a6987f31d40bf10cfa20fdbeae1aaccf9a4
Benjamin Neuville <b.neuville@outlook.fr>
2021-11-25 11:10:46 +0100

    Update SocialNetworkImpl.java

--- src/reseauSocial/core/SocialNetworkImpl.java
+++ src/reseauSocial/core/SocialNetworkImpl.java
@@ -1,56 +1,63 @@
 package reseauSocial.core;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Set;
 
 import facebookGhost.FacebookGhostNetwork;
 
-public class SocialNetworkImpl<T> implements SocialNetworkInterface<T> {
+public class SocialNetworkImpl<MemberInterface> implements SocialNetworkInterface<MemberInterface> {
+
+	ArrayList<MemberInterface> members = new ArrayList<MemberInterface>();
+	
+	@Override
+	public MemberInterface getMember(String identifier) {
+		for(MemberInterface m : members) {
+			if(((Member) m).getName() == identifier) {
+				return m;
+			}
+		}
+		return null;
+	}
 
 	@Override
-	public T getMember(String identifier) {
+	public Collection<MemberInterface> getMembers() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Collection<T> getMembers() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void addMember(T membre) {
+	public void addMember(MemberInterface membre) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public T addMember(String ident, FacebookGhostNetwork fg) {
+	public MemberInterface addMember(String ident, FacebookGhostNetwork fg) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public T addMember(String identifier) {
+	public MemberInterface addMember(String identifier) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void relate(Strength force, T member, T friend) {
+	public void relate(Strength force, MemberInterface member, MemberInterface friend) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public Set<T> relatedToRank(T member, int rank) {
+	public Set<MemberInterface> relatedToRank(MemberInterface member, int rank) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public int distance(T member1, T member2) {
+	public int distance(MemberInterface member1, MemberInterface member2) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
