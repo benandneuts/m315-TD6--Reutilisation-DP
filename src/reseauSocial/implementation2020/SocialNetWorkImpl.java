868ae210f5d08bd3b6010b319215d1543377150c
Benjamin Neuville <b.neuville@outlook.fr>
2021-12-01 13:57:28 +0100

    Update SocialNetWorkImpl.java

--- src/reseauSocial/implementation2020/SocialNetWorkImpl.java
+++ src/reseauSocial/implementation2020/SocialNetWorkImpl.java
@@ -2,6 +2,7 @@ package reseauSocial.implementation2020;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
 import facebookGhost.FacebookGhostNetwork;
@@ -60,24 +61,10 @@ public class SocialNetWorkImpl<MemberInterface> implements SocialNetworkInterfac
 		graphe.ajouterArc((Member) member, (Member) friend, force.getValue());
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	
 	public Set<MemberInterface> relatedToRank(MemberInterface member, int rank) {
-		ArrayList<MemberInterface> l = new ArrayList<>();
-		ArrayList<MemberInterface> res = new ArrayList<>();
-		if(rank <= 1) {
-			return ((Set<MemberInterface>) graphe.voisins((Member) member));
-		}
-		else {
-			for(int i = 0 ; i < graphe.voisins((Member) member).size(); i++) {
-				l.addAll((ArrayList<MemberInterface>) graphe.voisins((Member) member));
-			}
-			for(int i = 0; i < l.size(); i++) {
-				res.addAll(relatedToRank(l.get(i), rank-1));
-			}
-			return (Set<MemberInterface>) res;
-		}
+		return graphe.voisinsAuRang((Member) member, rank);
 	}
 
 	@Override
